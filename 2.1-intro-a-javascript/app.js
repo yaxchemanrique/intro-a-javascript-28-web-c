944d823c30c90aece68e22c91da5d71182bb3db5
Yaxche <yaxchemanrique@gmail.com>
2023-08-07 20:31:26 -0700

    feat: clase 1: Intro a Javascript

---
+++ 2.1-intro-a-javascript/app.js
@@ -0,0 +1,60 @@
+/*const number1 = "2";
+const number2 = 10;
+
+const sum = number1 + number2;
+const multiplcation = number1 * number2;
+const resta = number1 - number2;
+console.log(sum);
+console.log(multiplcation);
+
+console.log('La suma de ' + number1 + ' con ' + number2 + ' es igual a ' + sum);
+console.log('La resta de ' + number1 + ' con ' + number2 + ' es igual a ' + resta);
+*/
+/*
+const number1String = prompt('dame un numero:');
+const number2String = prompt('dame otro numero:');
+
+console.log(typeof number1String);
+console.log(typeof number2String);
+
+const number1  = Number(number1String);
+const number2  = Number(number2String);
+
+console.log(typeof number1);
+console.log(typeof number2);
+
+const sum = number1 + number2;
+const multiplcation = number1 * number2;
+const resta = number1 - number2;
+
+console.log('La suma de ' + number1 + ' con ' + number2 + ' es igual a ' + sum);
+console.log('La resta de ' + number1 + ' con ' + number2 + ' es igual a ' + resta);
+console.log('La multiplicación de ' + number1 + ' con ' + number2 + ' es igual a ' + multiplcation);
+*/
+
+// Declarar un variable con let o const
+// Asignar un valor a la variable con "="
+//let firstName1; //Declaracion de variable
+//firstName1 = 'Yaxche'; //Asignando un valor a la variable
+
+// const variable; no se vale, const tiene que ir incializado
+/*
+const firstName = prompt('Dime tu nombre');
+const birthYear = prompt('En qué año naciste?');
+
+alert('Hola ' + firstName + '!');
+
+const age = 2023 - Number(birthYear);
+
+alert('Tienes ' + age + ' años');
+*/
+// age = 50; -> No se puede reasignar porque es CONST
+
+const firstName= prompt('Dime tu nombre');
+const bithYear= prompt('En que año naciste?');
+
+alert('Hola' + firstName + '!');
+
+const age = 2023 - Number(birthYear);
+
+alert('Tienes' + age + 'años');

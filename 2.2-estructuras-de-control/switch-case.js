2cd157c9afa9e54c9813fce1a28008b78cd54f0f
Yaxche <yaxchemanrique@gmail.com>
2023-08-09 20:46:29 -0700

    feat: codigo de clase switch-case

---
+++ 2.2-estructuras-de-control/switch-case.js
@@ -0,0 +1,22 @@
+const dia = 'jueves';
+// Switch hace comparaciones ===
+switch (dia) {
+    case 'lunes':
+        console.log('Sumergirme en mi propia miseria');
+        break;
+    case 'martes':
+        console.log('contemplar el abismo');
+        break;
+    case 'miercoles':
+    case 'jueves':
+        console.log('solucionar el hambruna mundial!');
+        break;
+    default:
+        console.log('Danza y ejercicio');
+}
+
+/* if (dia >= 5) {
+    console.log('Sumergirme en mi propia miseria');
+} else {
+    console.log('contemplar el abismo');
+} */
